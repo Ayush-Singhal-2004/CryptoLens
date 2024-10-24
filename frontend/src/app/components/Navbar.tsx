544679aa159fc0dc679bcef75d9c13c3bbf78d10
Ayush-Singhal-2004 <ayushsinghal1904@gmail.com>
2024-10-24 11:33:15 +0530

    navbar-added

---
+++ frontend/src/app/components/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdOutlineLightMode } from "react-icons/md";
+import { useTheme } from "next-themes";
+
+function Navbar() {
+
+    const { theme, setTheme } = useTheme();
+    
+    const changeTheme = () => { 
+        setTheme(theme == "dark" ? "light" : "dark");
+    }
+
+    return (
+        <nav className="flex border-b dark:border-b-[#262626] shadow-md justify-between px-6 py-3">
+            <div className="flex gap-[5vw]">
+                <h1 className="text-3xl font-bold">CryptoLens</h1>
+                <Input type="text" placeholder="Search accounts" className="w-[32vw] shadow-sm" />
+            </div>
+            <div className="flex gap-3">
+                <Button variant="default" className="font-semibold">Connect Wallet</Button>
+                <Button variant="default" onClick={changeTheme}>
+                    {
+                        theme == "dark" ? <MdOutlineDarkMode /> : <MdOutlineLightMode />
+                    }
+                </Button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
