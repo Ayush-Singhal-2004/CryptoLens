fcf3792328ce9594008c4f4ad857b7750f053c6c
Muhammad Umer Asif <142830547+MuhammadUmerAsif@users.noreply.github.com>
2024-10-28 21:18:57 +0500

    add profile api's (#5)
    * add profile 3 api's
    
    * add chain api for home page

--- backend/src/controllers/web3.ts
+++ backend/src/controllers/web3.ts
@@ -1,12 +1,66 @@
 import { Request, Response, NextFunction } from "express";
-import axios from "axios";
 import { chains } from "../constants/constant";
 import { StatusCodes } from "http-status-codes";
-import {error as errorResponse ,success as successResponse} from "../utils/common"
+import {error as errorResponse ,success as successResponse,postRequest} from "../utils/common"
 
-export const getChains = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+// export interface ExtractedTokenData {
+//     exchange_rate: string;
+//     holders: string;
+//     icon_url: string;
+//     name: string;
+//     symbol: string;
+//   }
+
+export const getChains =async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         successResponse.data=chains
+        res.status(StatusCodes.OK).json(successResponse);
+    } catch (error: any) {
+        errorResponse.error = error
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(errorResponse);
+    }
+}
+
+export const getTokens = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const addressHash=req.params.address
+        let response= await postRequest("addon/763/rest/addresses/token-balances",addressHash)
+        // Note: Extract data from the api and populate the extreded_data array waiting for final the response discussion.
+        // let extreded_data: Array<ExtractedTokenData> = [];
+        // response.forEach((item: any) => {
+        //     extreded_data.push({
+        //         exchange_rate: item.token.exchange_rate,
+        //         holders: item.token.holders,
+        //         icon_url: item.token.icon_url,
+        //         name: item.token.name,
+        //         symbol: item.token.symbol,
+        //     });
+        // });
+        successResponse.data=response
+        res.status(StatusCodes.OK).json(successResponse);
+    } catch (error: any) {
+        errorResponse.error = error
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(errorResponse);
+    }
+};
+
+export const getNfts = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const addressHash=req.params.address
+        let result= await postRequest("addon/763/rest/addresses/nft",addressHash,"","ERC-721,ERC-404,ERC-1155")
+        successResponse.data=result
+        res.status(StatusCodes.OK).json(successResponse);
+    } catch (error: any) {
+        errorResponse.error = error
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(errorResponse);
+    }
+};
+
+export const getTransactionHistory = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const addressHash=req.params.address
+        let response= await postRequest("addon/763/rest/addresses/transactions",addressHash,"to | from","")
+        successResponse.data=response
         res.status(StatusCodes.OK).json(successResponse);
     } catch (error: any) {
         errorResponse.error = error
--- backend/src/routes/web3.ts
+++ backend/src/routes/web3.ts
@@ -1,7 +1,13 @@
 import express from "express";
-import { getChains } from "../controllers/web3";
+import { getChains,getTokens, getNfts,getTransactionHistory } from "../controllers/web3";
 const router = express.Router();
 
-router.get("/chains", getChains);
+// Home Page Api's
+router.get('/chains',getChains)
 
-export default router;  // Corrected export statement
+// Profile Api's
+router.get("/tokens/:address", getTokens);
+router.get("/nfts/:address", getNfts);
+router.get("/transactions/:address",getTransactionHistory);
+
+export default router; 
