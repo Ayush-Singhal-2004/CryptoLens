c1b7201431e7e593fe57631a5aaf4ee1fef86b2e
Muhammad Umer Asif <142830547+MuhammadUmerAsif@users.noreply.github.com>
2024-10-31 06:46:23 +0500

    off eslint

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,7 @@
 export default {
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint during builds
+  },
   images: {
     domains: ['zapper.xyz','static.coinpaprika.com','imagedelivery.net'], // Add any other domains you need
   },
